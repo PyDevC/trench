761b5d295d56739a6ed521ed3d0685ac5398fcc8
PyDevC <anuskcse@gmail.com>
2025-11-23 09:08:15 -0500

    updated setup.sh

--- runs/ubuntu/setup.sh
+++ runs/ubuntu/setup.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-sudo apt install tmux fzf -y
+sudo apt install rofi tmux fzf -y
+bash ~/trench/install/clipboard.sh
+bash ~/trench/install/zsh.sh
+bash ~/trench/install/vim.sh
+
+cp ~/trench/dotfiles/bin $HOME
+rm ~/.zshrc
+rm ~/.zsh_profile
+ln -s ~/trench/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/trench/dotfiles/zsh/.zsh_profile ~/.zsh_profile
+. ~/.zshrc
+cp ~/trench/dotfiles/config/nvim ~/.config/nvim
+cp ~/trench/dotfiles/config/tmux ~/.config/nvim
