4f86b6a1fbbd5e28a493fd9888254ee20f8122cd
PyDevC <anuskcse@gmail.com>
2025-11-24 09:25:30 -0500

    combined all container installation scripts into one

--- Containers/contgeneral.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-run fedora firefox gimp setup
---
+++ containerinstall.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+printf "Types of container installs:\npytorch\ndevenv\ngeneral\nllvm\nremoteuser\n"
+read -p "Enter one of the container type: " conttype
+echo $conttype
+
+if [[ $conttype == "devenv" ]]; then
+    read -p "Enter Container os name[ubuntu/fedora]: " os
+    run $os build neovim rocm setup core llama vulkan
+    runs/$os/python_env.sh ml pandas scikit-learn numpy matplotlib
+elif [[ $conttype == "pytorch" ]]; then
+    run setup pytorch rocm neovim buildsystem
+elif [[ $conttype == "general" ]]; then
+    run fedora firefox gimp setup
+elif [[ $conttype == "remoteuser" ]]; then
+    run setup
+elif [[ $conttype == "llvm" ]]; then
+    run ubuntu llvm setup buildsystem neovim
+fi
