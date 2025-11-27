57265305c3ced230fa237cadd4fd679714246755
PyDevC <anuskcse@gmail.com>
2025-11-27 09:06:51 -0500

    update apt or dnf before installing

--- containerinstall.sh
+++ containerinstall.sh
@@ -6,14 +6,23 @@ echo $conttype
 
 if [[ $conttype == "devenv" ]]; then
     read -p "Enter Container os name[ubuntu/fedora]: " os
+    if [[ $os == "ubuntu" ]]; then
+        sudo apt update -y
+    elif [[ $os == "fedora" ]]; then
+        sudo dnf update -y
+    fi
     ./run $os build neovim rocm setup core llama vulkan
     ./runs/$os/python_env.sh ml pandas scikit-learn numpy matplotlib
 elif [[ $conttype == "pytorch" ]]; then
+    sudo apt update -y
     ./run ubuntu setup pytorch rocm neovim buildsystem
 elif [[ $conttype == "general" ]]; then
+    sudo dnf update -y
     ./run fedora firefox gimp setup
 elif [[ $conttype == "remoteuser" ]]; then
+    sudo dnf update -y
     ./run fedora setup
 elif [[ $conttype == "llvm" ]]; then
+    sudo apt update -y
     ./run ubuntu llvm setup buildsystem neovim
 fi
