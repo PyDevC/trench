dcdf820d3abd1269ceb6c2118e50ad37421a8599
PyDevC <anuskcse@gmail.com>
2025-12-12 07:18:00 -0500

    noname update

--- config.sh
+++ config.sh
@@ -5,6 +5,5 @@ rm ~/.zshrc
 rm ~/.zsh_profile
 ln -s ~/trench/dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/trench/dotfiles/zsh/.zsh_profile ~/.zsh_profile
-. ~/.zshrc
 cp -r ~/trench/dotfiles/config/nvim ~/.config/
 cp -r ~/trench/dotfiles/config/tmux ~/.config/
