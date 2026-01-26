b1a8ebd55afdc19614388ec76524705ae9351b4e
PyDevC <anuskcse@gmail.com>
2026-01-26 05:52:13 -0500

    don't need these script in trench

--- install/hyprland.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo dnf copr enable solopasha/hyprland
-sudo dnf install -y hyprland hyprlock hyprpaper hyprshot wlogout-git
--- runs/ubuntu/ghostty.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
