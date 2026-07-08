8dd7b535add661c5d53ba729913de6b4f550c0a9
PyDevC <anuskcse@gmail.com>
2026-07-08 12:37:09 -0400

    updated llvm build flags and directory

--- runs/ubuntu/llvm.sh
+++ runs/ubuntu/llvm.sh
@@ -8,20 +8,29 @@ if [[ ! -d $repoloc ]];then
     git clone https://github.com/llvm/llvm-project.git $repoloc
 fi
 
-pushd $repoloc
-cmake -GNinja -Bbuild llvm \
+mkdir -p $repoloc/build
+pushd $repoloc/build
+cmake -G Ninja ../llvm \
     -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-    -DLLVM_ENABLE_PROJECTS="clang;lld;mlir;clang-tools-extra;compiler-rt;llvm;lldb;" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_COMPILER=clang++ \
+    -DCMAKE_C_COMPILER=clang \
+    -DLLVM_BUILD_EXAMPLES=ON \
+    -DLLVM_CCACHE_BUILD=ON \
+    -DLLVM_CCACHE_BUILD=ON \
+    -DLLVM_ENABLE_ASSERTIONS=ON \
+    -DLLVM_ENABLE_PROJECTS=mlir \
+    -DLLVM_PARALLEL_LINK_JOBS=3 \
+    -DLLVM_PARALLEL_TABLEGEN_JOBS=5 \
+    -DLLVM_TARGETS_TO_BUILD="Native;AMDGPU" \
+    -DLLVM_USE_LINKER=lld \
+    -DMLIR_AMDGPU_TARGET_CHIP=gfx1101 \
+    -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
+    -DMLIR_ENABLE_ROCM_CONVERSIONS=ON \
+    -DMLIR_ENABLE_ROCM_RUNNER=ON \
     -DPython3_FIND_VIRTUALENV=ONLY \
     -DPython_FIND_VIRTUALENV=ONLY \
-    -DMLIR_ENABLE_BINDINGS_PYTHON=ON \
-    -DLLVM_TARGETS_TO_BUILD=host \
-    -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
-    -DLLVM_USE_LINKER=lld \
-    -DLLVM_PARALLEL_LINK_JOBS=3 \
-    -DLLVM_CCACHE_BUILD=ON \
-    -DLLVM_PARALLEL_TABLEGEN_JOBS=5 \
-
+    -DROCM_PATH=/opt/rocm
 
 sudo cmake --build build
 popd
