f2d581a53eb78e9c7974e263a36ac018d088c504
PyDevC <anuskcse@gmail.com>
2026-07-18 01:31:57 -0400

    copy zsh_profile and zshrc instead of creating a symlink

--- config.sh
+++ config.sh
@@ -8,5 +8,5 @@ fi
 # sym link zsh config
 rm ~/.zshrc
 rm ~/.zsh_profile
-ln -s ~/trench/dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/trench/dotfiles/zsh/.zsh_profile ~/.zsh_profile
+cp ~/trench/dotfiles/zsh/.zshrc ~/.zshrc
+cp ~/trench/dotfiles/zsh/.zsh_profile ~/.zsh_profile
